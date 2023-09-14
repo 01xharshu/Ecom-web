dc6430954ceaab5cd914502489e82e47e165026c
01xans <shitansh.g2004@gmail.com>
2023-09-14 21:51:14 +0530

    home page done
    home page code done

--- ecom-app/src/App.js
+++ ecom-app/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h1>E-Study App</h1>
+      <Switch>
+        <Route path='/checkout'>
+          <Header />
+        </Route>
+        <Route path='/'>
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ ecom-app/src/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Header.css' 
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+function Header() {
+  return (
+    <div className='.header'>
+        <img className='header__logo' src="" />
+
+        <div className='header__search'>
+            <input className='header__searchInput' type='text' />
+            <SearchIcon className='header__searchIcon' />
+            {/* logo */ }
+        </div>
+        
+        <div className='header_nav'>
+            <div className='header__option'>
+                <span className='header__optionLineOne'>
+                    Hello Guest
+                </span>
+                <span className='header__optionLineTwo'>
+                    Sign In
+                </span>
+            </div>
+        
+            <div className='header__option'>
+            <   span className='header__optionLineOne'>
+                    Returns
+                </span>
+                <span className='header__optionLineTwo'>
+                    Orders
+                </span>
+            </div>
+
+            <div className='header__option'>
+                <span className='header__optionLineOne'>
+                    Your
+                </span>
+                <span className='header__optionLineTwo'>
+                    Prime 
+                </span>
+            </div>
+
+            <div className='header__optionBasket'>
+            <ShoppingBasketIcon /> 
+            <span className='header__optionLineTwo header__basketCount'>0</span>   
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
