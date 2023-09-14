0c287cd8bc07b7f2e9c88ab9d1b0b8dbec5f20bd
Harsh Mishra <108367606+01xharshu@users.noreply.github.com>
2023-09-14 18:41:50 +0530

    Create firebase.js

---
+++ ecom-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyByvS_EIcu0hdLwEKJ3Q32feZLBCTO-WX4",
+    authDomain: "ecom-app-2093e.firebaseapp.com",
+    projectId: "ecom-app-2093e",
+    storageBucket: "ecom-app-2093e.appspot.com",
+    messagingSenderId: "489004191396",
+    appId: "1:489004191396:web:8a4238dcf02fdef3bf2031",
+    measurementId: "G-88FZE8QFQZ"
+  };
